3b437790bd816fa12beda70d137d61119fea10c5
AK自动机 <47687653+boristown@users.noreply.github.com>
2026-09-15 19:38:09 +0800

    feat: route completed tasks to accuracy evaluation

--- web/src/router.ts
+++ web/src/router.ts
@@ -4,6 +4,7 @@ import PlaceholderView from './views/PlaceholderView.vue'
 import SystemView from './views/SystemView.vue'
 import TasksView from './views/TasksView.vue'
 import TaskWorkspace from './views/TaskWorkspace.vue'
+import TaskEvaluationView from './views/TaskEvaluationView.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -12,6 +13,7 @@ export const router = createRouter({
     { path: '/login', component: LoginView },
     { path: '/tasks', component: TasksView },
     { path: '/tasks/new', component: TaskWorkspace },
+    { path: '/tasks/:taskId/evaluation', component: TaskEvaluationView },
     { path: '/tasks/:taskId', component: TaskWorkspace },
     { path: '/profiles', component: PlaceholderView, props: { title: '匹配方案' } },
     { path: '/data', component: PlaceholderView, props: { title: '基础数据' } },
