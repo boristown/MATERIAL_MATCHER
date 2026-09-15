cb39c14ee393b1af3d07769b16ff6584a16e82f5
AK自动机 <47687653+boristown@users.noreply.github.com>
2026-09-15 20:49:50 +0800

    feat: 挂载匹配方案管理页面

--- web/src/router.ts
+++ web/src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from './views/LoginView.vue'
 import PlaceholderView from './views/PlaceholderView.vue'
+import ProfilesView from './views/ProfilesView.vue'
 import SystemView from './views/SystemView.vue'
 import TasksView from './views/TasksView.vue'
 import TaskWorkspace from './views/TaskWorkspace.vue'
@@ -15,7 +16,7 @@ export const router = createRouter({
     { path: '/tasks/new', component: TaskWorkspace },
     { path: '/tasks/:taskId/evaluation', component: TaskEvaluationView },
     { path: '/tasks/:taskId', component: TaskWorkspace },
-    { path: '/profiles', component: PlaceholderView, props: { title: '匹配方案' } },
+    { path: '/profiles', component: ProfilesView },
     { path: '/data', component: PlaceholderView, props: { title: '基础数据' } },
     { path: '/system', component: SystemView },
   ],
